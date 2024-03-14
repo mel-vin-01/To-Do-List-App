18d5674e2b5f260122fdfa51a9e012cc8dc79edf
Melvin Gamana Jr <melvingamanajr@gmail.com>
2024-03-14 16:35:10 +0800

    Add another methods

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import messagebox
+
 import customtkinter
 
 
@@ -7,12 +9,36 @@ class App(customtkinter.CTk):
 
         self.title("To-Do Application")
         self.geometry(f"{400}x{600}")
-
         self.main()
 
     def main(self):
-        self.container = customtkinter.CTkFrame(self)
-        self.container.pack()
+        container = customtkinter.CTkFrame(self)
+        container.pack(padx=5, pady=5, fill="x")
+
+        lbl_title = customtkinter.CTkLabel(container, text="To-Do List App", font=(customtkinter.CTkFont(weight="bold",
+                                                                                                         size=25)))
+        lbl_title.pack(pady=(20, 0))
+
+        self.txt_todo = customtkinter.CTkEntry(container, height=40, width=300)
+        self.txt_todo.pack(pady=(50, 5))
+
+        btn_submit = customtkinter.CTkButton(container, text="Submit", command=self.submit,
+                                             font=(customtkinter.CTkFont(weight="bold", size=20)))
+        btn_submit.pack(pady=(0, 10))
+
+        self.wrap = customtkinter.CTkScrollableFrame(self)
+        self.wrap.pack(pady=(5, 5), expand=True, fill="both")
+
+    def submit(self):
+        todo = self.txt_todo.get()
+
+        if not all([todo]):
+            messagebox.showerror("Error", "Please fill.")
+            return
+
+        chck_box = customtkinter.CTkCheckBox(self.wrap, text=todo, font=(customtkinter.CTkFont(size=18)))
+        chck_box.pack(pady=(3, 3), anchor="nw")
+
 
 if __name__ == "__main__":
     app = App()
